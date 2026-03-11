5899071f17e188c0f8809896168b24c44ab4fbf5
meankhoiii <khoiphan78757@gmail.com>
2026-03-11 14:56:25 +0700

    chore: clean up git tracking of build artifacts and update .gitignore

--- src/ChinhNha.Web/obj/Debug/net8.0/ChinhNha.Web.AssemblyInfo.cs
+++ src/ChinhNha.Web/obj/Debug/net8.0/ChinhNha.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChinhNha.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfcbeb852db3d6c47d272951547f7ba73d66b65a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a1eec21ba91a4958071fd092846e3f9092c8da")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChinhNha.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChinhNha.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
